73018271dd46f7fe32bc7484c80f83b7688b6d3e
Vinicius Fantinatto de Souza <viniciussouza@wareline.com.br>
2021-11-04 17:19:31 -0300

    update themes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'next-themes';
 
-import ThemeProvider from '../contexts/ThemeContext';
-import '../styles/global.scss';
+import '../styles/globals.scss';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
   return (
-    <ThemeProvider>
+    <ThemeProvider attribute="class" themes={['light', 'meio-dark', 'dark']}>
       <Component {...pageProps} />
     </ThemeProvider>
   );
 };
+
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,38 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import Head from 'next/head';
-
-import ThemeButton from '../components/ThemeButton';
+import Image from 'next/image';
+import { useTheme } from 'next-themes';
 
 import VercelLogo from '../assets/vercel.svg';
-
-import styles from '../styles/pages/Home.module.scss';
-
-import {
-  DEFAULT_THEME,
-  DARK_THEME,
-  LIGHT_THEME,
-  ThemeContext,
-} from '../contexts/ThemeContext';
+import styles from '../styles/Home.module.scss';
 
 const Home: React.FC = () => {
-  const { globalTheme } = useContext(ThemeContext);
+  const { theme, setTheme, systemTheme } = useTheme();
 
   return (
-    <div className={`${styles.container} ${styles[globalTheme]}`}>
+    <div>
       <Head>
         <title>Home page</title>
       </Head>
 
-      <VercelLogo style={{ background: '#fff' }} />
-      <h1 className={`${styles[globalTheme]}`}>Template Next.js</h1>
+      <Image src={VercelLogo} />
+      <h1>Template Next.js</h1>
       <p>Preparado com SASS, importação de imagens, temas, eslint e prettier</p>
-      <div>
-        <ThemeButton changeTheme={DEFAULT_THEME}>Default</ThemeButton>
-        <ThemeButton changeTheme={DARK_THEME}>Dark</ThemeButton>
-        <ThemeButton changeTheme={LIGHT_THEME}>Light</ThemeButton>
+
+      <div className={styles.buttonContainer}>
+        <h4>The current theme is: {theme}</h4>
+        <button type="button" onClick={() => setTheme(systemTheme ?? 'light')}>
+          Usar padrão do sistema
+        </button>
+        <button type="button" onClick={() => setTheme('light')}>
+          Claro
+        </button>
+        <button type="button" onClick={() => setTheme('meio-dark')}>
+          Meio Escuro
+        </button>
+        <button type="button" onClick={() => setTheme('dark')}>
+          Escuro
+        </button>
       </div>
     </div>
   );
